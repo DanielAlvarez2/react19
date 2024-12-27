fed98b431fcbee78a014bc37e7522ed6b8530d6b
Daniel Alvarez <daniel.yllanes@hotmail.com>
2024-12-27 10:04:46 -0500

    section 1 lesson 14 static page ReactFacts Custom Components part 2

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -7,15 +7,25 @@ function App() {
 function Page(){
   return(
     <>
-      <img src="react-logo.png" width="40px" alt="React logo" />    
-      <h1>Fun facts about React!</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <header>
+        <img src="react-logo.png" width="40px" alt="React logo" />    
+      </header>
+      <main>
+        <h1>Fun facts about React!</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+      <footer>
+        <small>
+          &copy;2024 ToggleSoftware.com<br />
+          All Rights Reserved.
+        </small>        
+      </footer>
     </>
   )
 }
