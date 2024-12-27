1723b7170366e81c92a77bcb3e2ef7e5a2db5ff6
Daniel Alvarez <daniel.yllanes@hotmail.com>
2024-12-27 13:33:50 -0500

    fcc React 19 Section 1 Lesson 17 Parent/Child Components

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -4,28 +4,46 @@ function App() {
   return <Page />
 }
 
+function Header() {
+  return(
+    <header>
+      <img src="react-logo.png" width="40px" alt="React logo" />    
+    </header>
+  )
+}
+
+function MainContent(){
+  return(
+    <main>
+    <h1>Fun facts about React!</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>
+      <small><small>
+        &copy;2024 ToggleSoftware.com<br />
+        All Rights Reserved.
+      </small></small>        
+    </footer>
+  )
+}
+
 function Page(){
   return(
     <>
-      <header>
-        <img src="react-logo.png" width="40px" alt="React logo" />    
-      </header>
-      <main>
-        <h1>Fun facts about React!</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
-      <footer>
-        <small>
-          &copy;2024 ToggleSoftware.com<br />
-          All Rights Reserved.
-        </small>        
-      </footer>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
